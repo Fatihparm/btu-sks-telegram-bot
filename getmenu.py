5f59e93fa240c5a49249bc8a6953c9731ec29f52
Fatih Parmaksız <fatihparm6@gmail.com>
2024-11-07 11:29:00 +0300

    getmenu.py updated

--- getmenu.py
+++ getmenu.py
@@ -1,4 +1,5 @@
-import fitz
+import fitz  # PyMuPDF
+import re
 import os
 
 dosya_dizini = os.path.dirname(os.path.abspath(__file__))
@@ -7,19 +8,28 @@ dosya_yolu = os.path.join(dosya_dizini, dosya_adi)
 
 class Menu():
     def __init__(self):
-      pass
+        pass
 
     def getFormattedMenu(self):
-      doc = fitz.open(dosya_yolu)
-      page = doc.load_page(0)
-      text = page.get_text("blocks")
+        # Blok verilerini al ve satırları düzenleyerek döndür
+        doc = fitz.open(dosya_yolu)
+        page = doc.load_page(0)
+        blocks = page.get_text("blocks")
 
-      liste = []
-      for a in range(2,len(text)):
-          try: 
-              int(text[a][4][0:2])
-          except:
-              continue
-          liste.append(text[a][4]) 
-      return liste
-    
+        # Blokları birleştirerek tabloyu oluştur
+        formatted_menu = []
+        temp = ""
+
+        for block in blocks[2:]:  # İlk birkaç blok başlık olabilir, onları atlıyoruz
+            text = block[4].strip()
+
+            if re.match(r"\d{2}\.\d{2}\.\d{4}", text):
+                if temp:
+                    formatted_menu.append(temp.strip())
+                    temp = ""
+            temp += text + " "  
+        if temp:
+            formatted_menu.append(temp.strip())
+        formatted_menu.pop(0)
+
+        return formatted_menu
