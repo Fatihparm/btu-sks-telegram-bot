09560485d1b454db5b0a44aacea06e0f4396c030
Fatih Parmaksız <fatihparm6@gmail.com>
2024-11-16 15:57:00 +0300

    pdf checkout

--- scrape.py
+++ scrape.py
@@ -1,17 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 from os import path
-
+import hashlib
 
 class ScrapeMenu:
 
   def __init__(self):
     pass
 
+  def calculate_hash(self, file_path):
+    with open(file_path, 'rb') as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return file_hash.hexdigest()
+
+  def is_pdf_updated(self, new_pdf_path):
+    old_hash_path = "pdf_hash.txt"
+
+    new_hash = self.calculate_hash(new_pdf_path)
+
+    if path.exists(old_hash_path):
+        with open(old_hash_path, "r") as f:
+            old_hash = f.read()
+        if new_hash == old_hash:
+            print("PDF değişmedi.")
+            return False
+
+    # pdf has changed
+    with open(old_hash_path, "w") as f:
+        f.write(new_hash)
+    print("PDF değişti.")
+    return True
+
   def getPdf(self):
     requests.packages.urllib3.disable_warnings()
-    if path.exists("pdf0.pdf"):
-      print("PDF dosyası zaten var")
+    if path.exists("pdf0.pdf") and not self.is_pdf_updated("pdf0.pdf"):
+      print("Mevcut PDF güncel.")
     else:
       url = "https://sks.btu.edu.tr/tr/sayfa/detay/4398/beslenme-ve-di̇yeteti̇k"
       response = requests.get(url, verify=False)
